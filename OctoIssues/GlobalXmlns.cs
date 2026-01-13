efe61604b6ed099e08bcccafa0d84898b569bd56
VV228VA <VV228VA@XW4KRXYP3>
2026-01-13 09:13:20 -0300

    Add project files.

---
+++ OctoIssues/GlobalXmlns.cs
@@ -0,0 +1,4 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "OctoIssues")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "OctoIssues.Pages")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "OctoIssues.ViewModels")]
+
